2b20db61e158193ba3bbfbd1168162bfa798b263
Sooshiko <86819845+Sooshiko@users.noreply.github.com>
2024-08-16 22:41:59 -0400

    1.0
    + Reworked fish class.

--- Client/Code/Other/Fish/Fish.gd
+++ Client/Code/Other/Fish/Fish.gd
@@ -7,7 +7,7 @@ var _Size_Modifier := 1.0
 static func Create(_species : String) -> Fish:
 	var _return = Fish.new()
 	_return._Species = _species
-	_return._Size_Modifier = randf_range(0.9, 1.1)
+	_return._Size_Modifier = randf_range(0.8, 1.2)
 	return _return
 
 func Get_Size() -> float: return Species.Get_Size(_Species) * _Size_Modifier
--- Client/Code/Other/Fish/Species.gd
+++ Client/Code/Other/Fish/Species.gd
@@ -2,21 +2,30 @@ extends Resource
 class_name Species
 
 const _Dictionary := {
-	"Test" : {
-		"SPEED" : 0.0,
-		"POWER" : 0.0,
-		"DEFENSE" : 0.0,
-		"AI" : [-1.0, -1.0, 2.0],
-		"TEXTURE" : "res://Resource/Texture/Game/Icon_Image.png",
+	"Test1" : {
+		"SPEED" : 1.0, #should never be more than like 3
+		"POWER" : 1.0, #should never be more tahn like 1.5
+		"DEFENSE" : 1.0, #should never be more than like 2
+		"AI" : [-1.0, -1.0, 2.5], #TODO need to implement AI
+		"TEXTURE" : "res://Resource/Texture/Fish/Test1.png",
 		"SIZE" : 1.0,
 		"PRICE" : 1.0,
+	},
+	"Test2" : {
+		"SPEED" : 2.0,
+		"POWER" : 1.5,
+		"DEFENSE" : 1.5,
+		"AI" : [1.0, -1.0, 1.5, -1.5],
+		"TEXTURE" : "res://Resource/Texture/Fish/Test2.png",
+		"SIZE" : 10.0,
+		"PRICE" : 10.0,
 	},
 }
 
 static func Get_Speed(_name : String) -> float: return _Dictionary[_name]["SPEED"]
 static func Get_Power(_name : String) -> float: return _Dictionary[_name]["POWER"]
 static func Get_Defense(_name : String) -> float: return _Dictionary[_name]["DEFENSE"]
-static func Get_AI(_name : String) -> float: return _Dictionary[_name]["AI"]
+static func Get_AI(_name : String) -> Array: return _Dictionary[_name]["AI"]
 static func Get_Texture(_name : String) -> String: return _Dictionary[_name]["TEXTURE"]
 static func Get_Size(_name : String) -> float: return _Dictionary[_name]["SIZE"]
 static func Get_Price(_name : String) -> float: return _Dictionary[_name]["PRICE"]
