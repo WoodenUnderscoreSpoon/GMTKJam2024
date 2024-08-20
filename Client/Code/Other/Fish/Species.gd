076ea03b87871b74cd6b6ce3304ade56f105da60
Sooshiko <86819845+Sooshiko@users.noreply.github.com>
2024-08-19 20:05:24 -0400

    1.0
    idk

--- Client/Code/Other/Fish/Species.gd
+++ Client/Code/Other/Fish/Species.gd
@@ -4,13 +4,13 @@ class_name Species
 const _Dictionary := {
 	#region Trash
 	"Penny" : {
-		"SPEED" : 0.0,
-		"POWER" : 0.0,
-		"DEFENSE" : 0.5,
+		"SPEED" : 0.0, #should never be more than like 3
+		"POWER" : 0.0, #should never be more tahn like 1.5
+		"DEFENSE" : 0.5, #should never be more than like 2
 		"AI" : [0.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Trash/Penny.png",
-		"SIZE" : 19.05,
-		"PRICE" : 0.01,
+		"SIZE" : 1.905, #in centimeters
+		"PRICE" : 0.01, #in dollars
 		"PERMIT" : 0,
 		"DESCRIPTION" : "",
 	},
@@ -20,75 +20,75 @@ const _Dictionary := {
 		"DEFENSE" : 0.5,
 		"AI" : [1.0, 0.0, -1.0, 0.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Trash/Dollar.png",
-		"SIZE" : 156.0,
+		"SIZE" : 15.6,
 		"PRICE" : 1.00,
 		"PERMIT" : 0,
 		"DESCRIPTION" : "",
 	},
 	"Cheese Fish" : {
-		"SPEED" : 0.7, #should never be more than like 3
-		"POWER" : 0.7, #should never be more tahn like 1.5
-		"DEFENSE" : 1.0, #should never be more than like 2
+		"SPEED" : 0.7,
+		"POWER" : 0.7,
+		"DEFENSE" : 1.0,
 		"AI" : [-1.0, -1.0, 0.0, 2.5],
 		"TEXTURE" : "res://Resource/Texture/Fish/Medium/Cheese Fish.png",
-		"SIZE" : 100.0,
-		"PRICE" : 1.50,
+		"SIZE" : 10.0,
+		"PRICE" : 2.00,
 		"PERMIT" : 0,
 		"DESCRIPTION" : "",
 	},
 	#endregion
 	#region Small
 	"Ancheddar" : {
-		"SPEED" : 0.8, #should never be more than like 3
-		"POWER" : 0.9, #should never be more tahn like 1.5
-		"DEFENSE" : 0.9, #should never be more than like 2
+		"SPEED" : 0.8,
+		"POWER" : 0.9,
+		"DEFENSE" : 0.9,
 		"AI" : [-1.0, 0.0, 1.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Small/Ancheddar.png",
-		"SIZE" : 200.0,
+		"SIZE" : 20.0,
 		"PRICE" : 4.25,
 		"PERMIT" : 1,
 		"DESCRIPTION" : "",
 	},
 	"Briem" : {
-		"SPEED" : 1.0, #should never be more than like 3
-		"POWER" : 1.0, #should never be more tahn like 1.5
-		"DEFENSE" : 0.9, #should never be more than like 2
+		"SPEED" : 1.0,
+		"POWER" : 1.0,
+		"DEFENSE" : 0.9,
 		"AI" : [-1.0, 1.0, 2.5, -1.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Small/Briem.png",
-		"SIZE" : 425.0,
+		"SIZE" : 42.5,
 		"PRICE" : 5.00,
 		"PERMIT" : 1,
 		"DESCRIPTION" : "",
 	},
 	"Curdchin" : {
-		"SPEED" : 0.8, #should never be more than like 3
-		"POWER" : 1.1, #should never be more tahn like 1.5
-		"DEFENSE" : 0.9, #should never be more than like 2
+		"SPEED" : 0.8,
+		"POWER" : 1.1,
+		"DEFENSE" : 0.9,
 		"AI" : [-1.0, 1.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Small/Curdchin.png",
-		"SIZE" : 180.0,
+		"SIZE" : 18.0,
 		"PRICE" : 4.00,
 		"PERMIT" : 1,
 		"DESCRIPTION" : "",
 	},
 	"Fetta Fish" : {
-		"SPEED" : 1.0, #should never be more than like 3
-		"POWER" : 1.0, #should never be more tahn like 1.5
-		"DEFENSE" : 0.9, #should never be more than like 2
+		"SPEED" : 1.0,
+		"POWER" : 1.0,
+		"DEFENSE" : 0.9,
 		"AI" : [-1.0, -1.0, -1.0, 2.5],
 		"TEXTURE" : "res://Resource/Texture/Fish/Small/FettaFish.png",
-		"SIZE" : 76.2,
+		"SIZE" : 7.62,
 		"PRICE" : 4.50,
 		"PERMIT" : 1,
 		"DESCRIPTION" : "",
 	},
 	"Sardanish" : {
-		"SPEED" : 1.0, #should never be more than like 3
-		"POWER" : 1.0, #should never be more tahn like 1.5
-		"DEFENSE" : 0.9, #should never be more than like 2
+		"SPEED" : 1.0,
+		"POWER" : 1.0,
+		"DEFENSE" : 0.9,
 		"AI" : [-1.5, 1.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Small/Sardanish.png",
-		"SIZE" : 220.0,
+		"SIZE" : 22.0,
 		"PRICE" : 4.75,
 		"PERMIT" : 1,
 		"DESCRIPTION" : "",
@@ -96,56 +96,56 @@ const _Dictionary := {
 	#endregion
 	#region Medium
 	"Codage Cheese" : {
-		"SPEED" : 1.0, #should never be more than like 3
-		"POWER" : 1.0, #should never be more tahn like 1.5
-		"DEFENSE" : 1.1, #should never be more than like 2
+		"SPEED" : 1.0,
+		"POWER" : 1.0,
+		"DEFENSE" : 1.1,
 		"AI" : [-1.0, -1.0, 2.5],
 		"TEXTURE" : "res://Resource/Texture/Fish/Medium/CoddageCheese.png",
-		"SIZE" : 863.6,
+		"SIZE" : 186.36,
 		"PRICE" : 10.00,
 		"PERMIT" : 2,
 		"DESCRIPTION" : "",
 	},
 	"Colby Jackfish" : {
-		"SPEED" : 0.9, #should never be more than like 3
-		"POWER" : 1.2, #should never be more tahn like 1.5
-		"DEFENSE" : 1.1, #should never be more than like 2
+		"SPEED" : 0.9,
+		"POWER" : 1.2,
+		"DEFENSE" : 1.1,
 		"AI" : [-1.0, 0.5, 2.5],
 		"TEXTURE" : "res://Resource/Texture/Fish/Medium/ColbyJackfish.png",
-		"SIZE" : 1010.0,
+		"SIZE" : 201.0,
 		"PRICE" : 12.00,
 		"PERMIT" : 2,
 		"DESCRIPTION" : "",
 	},
 	"Emmentalapia" : {
-		"SPEED" : 1.2, #should never be more than like 3
-		"POWER" : 1.0, #should never be more tahn like 1.5
-		"DEFENSE" : 1.0, #should never be more than like 2
+		"SPEED" : 1.2,
+		"POWER" : 1.0,
+		"DEFENSE" : 1.0,
 		"AI" : [-1.0, -1.0, -1.5, 2.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Medium/Emmentalapia.png",
-		"SIZE" : 533.4,
+		"SIZE" : 153.34,
 		"PRICE" : 8.00,
 		"PERMIT" : 2,
 		"DESCRIPTION" : "",
 	},
 	"Raycotta" : {
-		"SPEED" : 1.2, #should never be more than like 3
-		"POWER" : 1.3, #should never be more tahn like 1.5
-		"DEFENSE" : 1.3, #should never be more than like 2
+		"SPEED" : 1.2,
+		"POWER" : 1.3,
+		"DEFENSE" : 1.3,
 		"AI" : [-1.0, -1.0, 2.5],
 		"TEXTURE" : "res://Resource/Texture/Fish/Medium/Raycotta.png",
-		"SIZE" : 3096.0,
+		"SIZE" : 409.60,
 		"PRICE" : 11.00,
 		"PERMIT" : 2,
 		"DESCRIPTION" : "",
 	},
 	"Salmontforte" : {
-		"SPEED" : 1.0, #should never be more than like 3
-		"POWER" : 1.3, #should never be more tahn like 1.5
-		"DEFENSE" : 1.2, #should never be more than like 2
+		"SPEED" : 1.0,
+		"POWER" : 1.3,
+		"DEFENSE" : 1.2,
 		"AI" : [-1.0, -1.0, 3.0, -1.0],
 		"TEXTURE" : "res://Resource/Texture/Fish/Medium/Salmontforte.png",
-		"SIZE" : 1473.2,
+		"SIZE" : 247.32,
 		"PRICE" : 15.00,
 		"PERMIT" : 2,
 		"DESCRIPTION" : "",
